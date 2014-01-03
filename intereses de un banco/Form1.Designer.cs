5dab8ed479d3f056d2b49417c650360dee5cdca7
AlexJPZ17 <alexjpz17@gmail.com>
2014-01-02 22:43:13 -0600

    C Sharp intereses de un banco

---
+++ intereses de un banco/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace intereses_de_un_banco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Close = new System.Windows.Forms.Button();
+            this.button_Run = new System.Windows.Forms.Button();
+            this.textBox_Invesion = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_Interes = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Inicial = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_Close
+            // 
+            this.button_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Close.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Close.Location = new System.Drawing.Point(320, 1);
+            this.button_Close.Name = "button_Close";
+            this.button_Close.Size = new System.Drawing.Size(40, 26);
+            this.button_Close.TabIndex = 19;
+            this.button_Close.Text = "X";
+            this.button_Close.UseVisualStyleBackColor = true;
+            this.button_Close.Click += new System.EventHandler(this.button_Close_Click);
+            // 
+            // button_Run
+            // 
+            this.button_Run.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Run.Location = new System.Drawing.Point(136, 288);
+            this.button_Run.Name = "button_Run";
+            this.button_Run.Size = new System.Drawing.Size(75, 23);
+            this.button_Run.TabIndex = 18;
+            this.button_Run.Text = "Run";
+            this.button_Run.UseVisualStyleBackColor = true;
+            this.button_Run.Click += new System.EventHandler(this.button_Run_Click);
+            // 
+            // textBox_Invesion
+            // 
+            this.textBox_Invesion.Location = new System.Drawing.Point(124, 186);
+            this.textBox_Invesion.Name = "textBox_Invesion";
+            this.textBox_Invesion.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Invesion.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(116, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 15);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Meses de inversion";
+            // 
+            // textBox_Interes
+            // 
+            this.textBox_Interes.Location = new System.Drawing.Point(211, 111);
+            this.textBox_Interes.Name = "textBox_Interes";
+            this.textBox_Interes.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Interes.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(214, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 15);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Interes mensual";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Capital inicial ";
+            // 
+            // textBox_Inicial
+            // 
+            this.textBox_Inicial.Location = new System.Drawing.Point(48, 111);
+            this.textBox_Inicial.Name = "textBox_Inicial";
+            this.textBox_Inicial.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Inicial.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Bancon Central";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(368, 364);
+            this.Controls.Add(this.button_Close);
+            this.Controls.Add(this.button_Run);
+            this.Controls.Add(this.textBox_Invesion);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_Interes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_Inicial);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Close;
+        private System.Windows.Forms.Button button_Run;
+        private System.Windows.Forms.TextBox textBox_Invesion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_Interes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Inicial;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ intereses de un banco/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace intereses_de_un_banco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        static double calcular(double a, double b, double c)
+        {
+            double total, total1 = 0;
+            double t = b / 100;
+            for (int d = 0; d < c ; d++)
+            {
+                total = a * t;
+                total1 = total1 + total;
+            }
+            return total1;
+        }
+        public void Capital()
+        {
+            double capital;
+            double interes;
+            double mes;
+
+            capital = Convert.ToDouble(textBox_Inicial.Text);
+            if (capital < 500)
+            {
+                MessageBox.Show(capital + " Capital erroneo", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                interes = Convert.ToDouble(textBox_Interes.Text);
+                if (interes > 8 || interes < 1)
+                {
+                    MessageBox.Show(interes + " Taza de interes erronea", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    mes = Convert.ToDouble(textBox_Invesion.Text);
+                    if (mes > 12 || mes < 1)
+                    {
+                        MessageBox.Show(mes + " Numeros de meses erroneo", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        double x = calcular(capital, interes, mes);
+                        MessageBox.Show(" Su interes generado en " + mes + " meses es " + x);
+                        double capt = x + capital;
+                        MessageBox.Show("Su capital total es " + capt);
+                    }
+                }
+            }
+        }
+
+        private void button_Run_Click(object sender, EventArgs e)
+        {
+            Capital();
+        }
+    }
+}
